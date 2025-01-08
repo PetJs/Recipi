6416e541a08fa9ce353e9e5c9eda6f914aae95a0
PetJs <peterfagoroye76@gmail.com>
2025-01-08 12:43:47 +0100

    Added Hero Section

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -6,6 +6,9 @@ import Facebook from "../assets/images/001-facebook.svg"
 import Twitter from "../assets/images/003-twitter.svg"
 import NavBar from "@/components/layouts/navbar"
 import randomBg from "../assets/images/foofBg.jpg"
+import img from "../assets/images/image 14.png"
+import Timer from "../assets/images/Timer.svg"
+import Forknife from "../assets/images/ForkKnife.svg"
 import { Random } from "@/types"
 
 
@@ -30,7 +33,7 @@ function HomePage() {
     }
 
     useEffect(() => {
-        // getRandomRecipe()
+        getRandomRecipe()
     }, [])
 
 
@@ -43,18 +46,28 @@ function HomePage() {
                 Insta={Insta}
             />
             {/* HERO SECTION */}
-            <div className="relative flex bg-red-500 m-4 justify-between h-96">
-                <div className="bg-blue-700 w-[50%] rounded-2xl rounded-r">
-                    <h1>{random?.title || "Spicy delicous COder"}</h1>
-                    <p dangerouslySetInnerHTML={{ __html: random?.summary || "lorem ipsum jare" }} />
-                    <div>
-                        <p>{random?.readyInMinutes || 20}</p>
-                        <p>{random?.diets[0] || "Chicken"}</p>
+            <div className="relative flex m-4 justify-between md:h-96 ">
+                <div className="w-[50%] bg-[#E7FAFE] rounded-2xl rounded-r p-2">
+                    <div className="flex rounded-lg bg-[#FFFFFF] w-fit items-center ">
+                        <img src={img} alt="" className="w-3 h-3"/>
+                        <h2 className="text-[16px] ">Hot Recipes</h2>
+                    </div>
+                    <h1 className="font-bold mdtext-xl text-lg max-w-2xl">{random?.title}</h1>
+                    <p dangerouslySetInnerHTML={{ __html: random?.summary || "" }} className="text-sm line-clamp-4"/>
+                    <div className="flex md:gap-6 gap-2 mt-2">
+                        <div className="flex items-center bg-[#00000099] w-fit rounded-lg md:px-2 px-1">
+                            <img src={Timer} alt="" className="w-3 h-3"/>
+                            <p className="md:text-sm text-[12px] whitespace-nowrap">{random?.readyInMinutes } mins</p>
+                        </div>
+                        <div className="flex items-center bg-[#00000099] w-fit rounded-lg md:px-2 px-1 ">
+                            <img src={Forknife} alt="" className="w-3 h-3"/>
+                            <p className="md:text-sm text-[12px] whitespace-nowrap">{random?.diets[0] }</p>
+                        </div>
                     </div>                    
                 </div>
                 <div className="w-[50%] relative ">
                     <img src={randomBg} alt="" className="w-full h-full rounded-2xl rounded-l" />
-                    <img src={random?.image || "https://img.spoonacular.com/recipes/639779-556x370.jpg"} alt="" className="absolute inset-0 m-14 w-64 h-64 rounded-full border border-gray-900 " />
+                    <img src={random?.image || "https://img.spoonacular.com/recipes/639779-556x370.jpg"} alt="" className="absolute inset-0 md:m-14 md:w-64 md:h-64 h-24 w-24 m-6 rounded-full border border-gray-900 " />
                 </div>
             </div>
             
