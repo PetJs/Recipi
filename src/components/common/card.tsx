67d804e27beb2f47ece53d3c556579775ca12fe2
PetJs <peterfagoroye76@gmail.com>
2025-03-10 14:36:43 +0100

    style: :lipstick: Updates Responsiveness

--- src/components/common/card.tsx
+++ src/components/common/card.tsx
@@ -13,8 +13,8 @@ const Card: React.FC<CardProps> = ({img, title, time, food}) =>{
         <div className="flex flex-col justify-center  bg-gradient-to-t from-[#E7F9FD] to-[#E7F9FD]/10 text-black items-center p-3 rounded-lg">
             <img src={typeof src === 'string' ? src : src.src} alt={alt} className={`md:w-full md:h-auto w-32 rounded-lg ${className}`} />
             <div>
-                <p className="text-ellipsis whitespace-pre-wrap w-[14rem] line-clamp-2 text-lg font-bold">{title}</p>
-                <div className="flex md:gap-6 gap-2 mt-2">
+                <p className="text-ellipsis whitespace-pre-wrap w-[14rem] line-clamp-2 md:text-lg text-[12px] font-bold text-center">{title}</p>
+                <div className="flex md:gap-6 gap-2 mt-2 justify-center">
                         <div className="flex items-center bg-[#00000099] w-fit rounded-lg md:px-2 px-1">
                             <img src={Timer} alt="" className="w-3 h-3"/>
                             <p className="md:text-sm text-[12px] whitespace-nowrap">{time} mins</p>
--- src/components/common/listCard.tsx
+++ src/components/common/listCard.tsx
@@ -4,7 +4,7 @@ import { ListCardProps } from "@/types";
 const ListCard: React.FC<ListCardProps> = ({img, text, onClick}) => {
     const {src, alt, className =''} = img;
     return (
-        <div onClick={onClick ? () => onClick() : undefined} className="md:w-20 md:h-20 w-16 flex flex-col items-center rounded-lg bg-gradient-to-t from-[#708246] to-[#708246]/10 mb-2">
+        <div onClick={onClick ? () => onClick() : undefined} className="md:w-20 md:h-20 w-16 flex flex-col items-center rounded-lg bg-gradient-to-t from-[#708246] to-[#708246]/10 mb-2 cursor-pointer">
             <img src={typeof src === 'string' ? src : src.src} alt={alt} className={`rounded-lg md:w-12 md:h-12 w-8 h-8 m-1 ${className}`} />
             <p className="text-white md:text-lg text-sm">{text}</p>
         </div>
