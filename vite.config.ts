0c35b993cecb1271f169a1f43070761e077a2f5d
PetJs <peterfagoroye76@gmail.com>
2025-04-10 13:36:31 +0100

    refactor: :construction_worker: Configure port and server

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: '0.0.0.0',  // Listen on all network interfaces
+    port: 5173,        // You can change the port if you prefer
+  },
 })
