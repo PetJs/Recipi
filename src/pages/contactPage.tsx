02d4db91e82a0ba6852f4298a6508bb626790621
PetJs <peterfagoroye76@gmail.com>
2025-04-11 15:58:44 +0100

    fix: Modify

--- src/pages/contactPage.tsx
+++ src/pages/contactPage.tsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from "react"
-import axiosInstance from "@/api/axios"
-import Logo from "../assets/svgs/Cooking Template 🟣 by Flowbase.co (Community).svg"
-import Insta from "../assets/svgs/004-instagram.svg"
-import Facebook from "../assets/svgs/001-facebook.svg"
-import Twitter from "../assets/svgs/003-twitter.svg"
-import NavBar from "@/components/common/navbar"
+
 
 function ContactPage(){
 
 
     return(
         <div className="h-screen">
-            <NavBar 
-                Logo={Logo}
-                Facebook={Facebook}
-                Twitter={Twitter}
-                Insta={Insta}
-            />
+            <h2>In Progress</h2>
         </div>
     )
 }
