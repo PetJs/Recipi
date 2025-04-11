e9c7612b1fc61a04d61bc0a26bce356629805436
PetJs <peterfagoroye76@gmail.com>
2025-04-11 15:51:32 +0100

    refactor: :recycle: Refactors userStor to persist local storage

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,12 +1,19 @@
-// src/store/userStore.ts
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface UserStore {
   selectedRecipeId: number | null;
   setSelectedRecipeId: (id: number) => void;
 }
 
-export const useUserStore = create<UserStore>((set) => ({
-  selectedRecipeId: null,
-  setSelectedRecipeId: (id) => set({ selectedRecipeId: id }),
-}));
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      selectedRecipeId: null,
+      setSelectedRecipeId: (id) => set({ selectedRecipeId: id }),
+    }),
+    {
+      name: "user-store", // localStorage key
+    }
+  )
+);
