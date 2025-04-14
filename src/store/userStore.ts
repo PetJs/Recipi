c3ec5236695c4411a2a44bea556c1803edc46faf
PetJs <peterfagoroye76@gmail.com>
2025-04-14 14:39:34 +0100

    feat: :sparkles: Updates UserStore

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,14 +1,34 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { User } from "@/types";
+import { create, StateCreator } from "zustand";
+import { persist, PersistOptions } from "zustand/middleware";
 
 interface UserStore {
   selectedRecipeId: number | null;
   setSelectedRecipeId: (id: number) => void;
+  user: User | null;
+  setUser: (data: { user: User }) => void;
+  accessHash: string | null;
+  refreshHash: string | null;
+  setHash: (accessHash: string, refreshHash: string) => void;
 }
 
+type MyPersist = (
+  config: StateCreator<UserStore>,
+  options: PersistOptions<UserStore>
+) => StateCreator<UserStore>;
+
 export const useUserStore = create<UserStore>()(
-  persist(
+  (persist as MyPersist)(
     (set) => ({
+      user: null,
+      accessHash: null,
+      refreshHash: null,
+      setHash: (accessHash, refreshHash) =>
+        set({ accessHash, refreshHash }),
+      setUser: ({ user }) =>
+        set({
+          user,
+        }),
       selectedRecipeId: null,
       setSelectedRecipeId: (id) => set({ selectedRecipeId: id }),
     }),
